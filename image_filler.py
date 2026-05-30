e45a2c8515f700ad5ad13f099ecc58b54306315b
andrzhirin-code <andr.zhirin@gmail.com>
2026-05-30 13:42:38 +0500

    Update image_filler.py

--- image_filler.py
+++ image_filler.py
@@ -3,7 +3,7 @@ import asyncio
 from PIL import Image, ImageDraw, ImageFont
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FONT_PATH = os.path.join(BASE_DIR, "arial.ttf")
+FONT_PATH = os.path.join(BASE_DIR, "ARIAL.TTF")
 TEMPLATE_PATH = os.path.join(BASE_DIR, "template.png")
 
 def generate_pdf(data: dict) -> str:
@@ -17,19 +17,16 @@ def generate_pdf(data: dict) -> str:
 
     font = ImageFont.truetype(FONT_PATH, size=70)
 
-    # Координаты из твоих замеров
     x = 977
     y_start = 1086
     y_step = 195
 
-    # ========== ОТЛАДОЧНЫЕ МЕТКИ (удалишь потом) ==========
+    # Отладочные метки
     draw.line((0, y_start, img.width, y_start), fill="red", width=5)
     draw.line((x, 0, x, img.height), fill="blue", width=5)
     draw.ellipse([x-15, y_start-15, x+15, y_start+15], fill=(255, 0, 0))
     draw.text((x, y_start), "Тест (Тип ТС) урд", fill=(0, 0, 0), font=font, anchor="ls")
-    # =====================================================
 
-    # Сохраняем PDF
     output_path = os.path.join(BASE_DIR, f"order_{data.get('id', 'temp')}.pdf")
     img.save(output_path, "PDF", resolution=300.0)
     return output_path
