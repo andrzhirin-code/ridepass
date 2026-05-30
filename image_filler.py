317826a0d495be92d6ceddff30c836859c001648
andrzhirin-code <andr.zhirin@gmail.com>
2026-05-30 23:50:34 +0500

    Update image_filler.py

--- image_filler.py
+++ image_filler.py
@@ -1,19 +1,8 @@
 import os
 import fitz
-import requests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, "template_form.pdf")
-ADMIN_ID = 5171781123
-BOT_TOKEN = "8223376010:AAEzIB8EZqZexiOv8bzhhJLyv7fwO2Afte4"
-
-def send_telegram(text: str):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {"chat_id": ADMIN_ID, "text": text}
-    try:
-        requests.post(url, data=data)
-    except Exception as e:
-        print(f"Ошибка отправки: {e}")
 
 def fill_order_template(data: dict) -> str:
     if not os.path.exists(TEMPLATE_PATH):
@@ -22,14 +11,14 @@ def fill_order_template(data: dict) -> str:
     doc = fitz.open(TEMPLATE_PATH)
     page = doc[0]
 
-    widgets = list(page.widgets())
-    msg = f"Найдено виджетов: {len(widgets)}\n"
-    for w in widgets:
-        msg += f"Поле: {w.field_name}\n"
+    # Диагностика: пишем в файл (потом скачаем)
+    with open("debug.txt", "w") as f:
+        widgets = list(page.widgets())
+        f.write(f"Найдено виджетов: {len(widgets)}\n")
+        for w in widgets:
+            f.write(f"Поле: {w.field_name}\n")
 
-    send_telegram(msg)
-
-    output_path = os.path.join(BASE_DIR, f"debug.pdf")
+    output_path = os.path.join(BASE_DIR, "debug.pdf")
     doc.save(output_path)
     doc.close()
     return output_path
